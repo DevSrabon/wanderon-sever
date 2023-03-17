843b7633fca999b415b1eb24588a169547a83e26
DevSrabon <et.srabon@gmail.com>
2023-03-17 20:24:06 +0600

    addded upcoming trips api with shortbydate

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const express = require('express');
+const express = require("express");
 
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { query } = require('express');
+const { query } = require("express");
 
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 // Database Connection
-const uri =
-	`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sajc8ea.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sajc8ea.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-})
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
 
+async function main() {
+	try {
+		const db = client.db("wanderonClone");
+		const upcomingCollection = db.collection("upcoming");
 
-async function main() { 
-    try {
-        
-    }
-    finally {
-        
-    }
+		app.get("/upcoming", async (req, res) => {
+			const options = req.query.option;
+			let query = {};
+			if (options.length) {
+				query = {
+					$text: {
+						$search: options,
+					},
+				};
+			}
+			res.send(await upcomingCollection.find(query).toArray());
+		});
+	} finally {
+	}
 }
-main().catch(err => console.dir(err));
+main().catch((err) => console.dir(err));
 
-app.get('/', (req, res) => {
-res.send('Server is working!');
-})
+app.get("/", (req, res) => {
+	res.send("Server is working!");
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
